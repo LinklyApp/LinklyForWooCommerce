90a770ced77381848f04ea8c01c0167633e2f602
msthu <mischa@b302.nl>
2021-02-14 20:30:57 +0100

    Update composer and mapper

--- includes/helpers/class-b-customer-to-wc-customer-mapper.php
+++ includes/helpers/class-b-customer-to-wc-customer-mapper.php
@@ -8,12 +8,24 @@ class BCustomerToWCCustomerMapper
             'email' => $data['email'],
             'first_name' => $data['firstName'],
             'last_name' => self::createLastName($data['familyNameInfix'], $data['familyName']),
+
+            'billing_email' => $data['email'],
             'billing_first_name' => $data['firstName'],
             'billing_last_name' => self::createLastName($data['familyNameInfix'], $data['familyName']),
-            'billing_address_1' => self::createAddress($data['street'], $data['houseNumber'], $data['houseNumberSuffix']),
-            'billing_city' => $data['city'],
-            'billing_postcode' => $data['postcode'],
-            'billing_country' => $data['country_code_short'],
+            'billing_address_1' => self::createAddress($data['billing']['street'], $data['billing']['houseNumber'],
+                $data['billing']['houseNumberSuffix']),
+            'billing_city' => $data['billing']['city'],
+            'billing_postcode' => $data['billing']['postcode'],
+            'billing_country' => $data['billing']['country']['alpha2'],
+
+            'shipping_first_name' => $data['firstName'],
+            'shipping_last_name' => self::createLastName($data['familyNameInfix'], $data['familyName']),
+            'shipping_address_1' => self::createAddress($data['shipping']['street'], $data['shipping']['houseNumber'],
+                $data['shipping']['houseNumberSuffix']),
+            'shipping_city' => $data['shipping']['city'],
+            'shipping_postcode' => $data['shipping']['postcode'],
+            'shipping_country' => $data['shipping']['country']['alpha2'],
+
             'sso_version' => $data['version'],
         ];
     }
