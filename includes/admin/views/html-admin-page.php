95f707f67397d6f08a1c8fe2f0e41d750b38eb9e
Jordan Geurtsen <j.geurtsen1@student.han.nl>
2023-06-02 01:14:26 +0200

    Last tweaks and fixes to admin page

--- includes/admin/views/html-admin-page.php
+++ includes/admin/views/html-admin-page.php
@@ -58,14 +58,18 @@ $logoStyle   = get_option( 'linkly_button_style' ) === 'primary' ? 'light' : 'da
 			</label>
 			<input name="linkly_client_secret" id="linkly_client_secret" class="linkly-form-input" type="password"
 			       value="<?= get_option( 'linkly_settings_app_secret' ) ?>" disabled />
-            <span class="linkly-secret-eye"><i id="toggler"><img src="<?= LINKLY_FOR_WOOCOMMERCE_PLUGIN_URL . "assets/images/password-eye-open.svg" ?>"></i></span>
+            <span class="linkly-secret-eye"><i id="passwordToggler"><img src="<?= LINKLY_FOR_WOOCOMMERCE_PLUGIN_URL . "assets/images/password-eye-open.svg" ?>"></i></span>
         </div>
         <div id="linkly_credentials_button" class="linkly-credentials-button">
-            <div id="linkly_credential_edit_button">
-                <button class="button-primary" type="button">
-                    <?= LinklyLanguageHelper::instance()->get( "edit_credentials" ); ?>
-                </button>
-            </div>
+            <button id="linkly_edit_credential_button" class="button-primary" type="button">
+                <?= LinklyLanguageHelper::instance()->get( "edit_credentials" ); ?>
+            </button>
+            <button id="linkly_save_credentials_button" class="button-primary" type="submit">
+                    <?= LinklyLanguageHelper::instance()->get( "save_changes" ); ?>
+            </button>
+            <button id="linkly_cancel_edit_credential_button" class="button-primary" type="button">
+                    <?= LinklyLanguageHelper::instance()->get( "cancel" ); ?>
+            </button>
         </div>
 	</form>
 	<form method="post">
@@ -74,75 +78,82 @@ $logoStyle   = get_option( 'linkly_button_style' ) === 'primary' ? 'light' : 'da
         <p>
 	        <?= LinklyLanguageHelper::instance()->get('button_style.change') ?>
         </p>
-        <button type="submit" name="linkly_button_style" class="linkly-button primary" value="primary">
-            <a target="_blank"><span><?= LinklyLanguageHelper::instance()->get( "button_style.primary" ) ?></span>
-                <img src="<?= LINKLY_FOR_WOOCOMMERCE_PLUGIN_URL . "assets/images/logo-horizontal-light.svg" ?>"></a>
-        </button>
-        <button type="submit" name="linkly_button_style" class="linkly-button secondary" value="secondary">
-            <a target="_blank"><span><?= LinklyLanguageHelper::instance()->get( "button_style.secondary" ) ?></span>
-                <img src="<?= LINKLY_FOR_WOOCOMMERCE_PLUGIN_URL . "assets/images/logo-horizontal-dark.svg" ?>" alt="Linkly"></a>
-        </button>
+        <div class="linkly-form-group">
+            <button type="submit" name="linkly_button_style" class="linkly-button primary" value="primary">
+                <a target="_blank"><span><?= LinklyLanguageHelper::instance()->get( "button_style.primary" ) ?></span>
+                    <img src="<?= LINKLY_FOR_WOOCOMMERCE_PLUGIN_URL . "assets/images/logo-horizontal-light.svg" ?>" alt="Linkly"></a>
+            </button>
+            <p class="linkly-button-current">
+	            <?= get_option( 'linkly_button_style' ) === 'primary' ? LinklyLanguageHelper::instance()->get('button_style.current') : '' ?>
+            </p>
+        </div>
+        <div class="linkly-form-group">
+            <button type="submit" name="linkly_button_style" class="linkly-button secondary" value="secondary">
+                <a target="_blank"><span><?= LinklyLanguageHelper::instance()->get( "button_style.secondary" ) ?></span>
+                    <img src="<?= LINKLY_FOR_WOOCOMMERCE_PLUGIN_URL . "assets/images/logo-horizontal-dark.svg" ?>" alt="Linkly"></a>
+            </button>
+            <p class="linkly-button-current">
+	            <?= get_option( 'linkly_button_style' ) === 'secondary' ? LinklyLanguageHelper::instance()->get('button_style.current') : '' ?>
+            </p>
+        </div>
 	</form>
 
     <script>
-        const clientId = document.getElementById('linkly_client_id');
-        const clientSecret = document.getElementById('linkly_client_secret');
-        const editButton = document.getElementById('linkly_credential_edit_button').getElementsByTagName('button')[0];
-        const credentialsButton = document.getElementById('linkly_credentials_button');
-        const toggler = document.getElementById('toggler');
-        let edit = false;
+        var clientId = document.getElementById('linkly_client_id');
+        var clientSecret = document.getElementById('linkly_client_secret');
+        var editButton = document.getElementById('linkly_edit_credential_button');
+        var saveButton = document.getElementById('linkly_save_credentials_button');
+        var cancelButton = document.getElementById('linkly_cancel_edit_credential_button');
+        var passwordToggler = document.getElementById('passwordToggler');
+        var edit = false;
 
-        if (clientId.value === '' && clientSecret.value === '') {
+        saveButton.style.display = 'none';
+        cancelButton.style.display = 'none';
+
+        if (clientId.value === '' || clientSecret.value === '') {
 
             clientId.removeAttribute('disabled');
             clientSecret.removeAttribute('disabled');
-            editButton.remove();
 
-            const saveButton = document.createElement('button');
-            saveButton.setAttribute('class', 'button-primary');
-            saveButton.setAttribute('type', 'submit');
-            saveButton.innerText = '<?= LinklyLanguageHelper::instance()->get( "save_changes" ) ?>';
-            credentialsButton.appendChild(saveButton);
+            editButton.style.display = 'none';
+            saveButton.style.display = 'inline-block';
+            cancelButton.style.display = 'none';
         }
 
-        showHidePassword = () => {
+        function showHidePassword() {
             if (clientSecret.type === 'password') {
                 clientSecret.setAttribute('type', 'text');
-                toggler.getElementsByTagName('img')[0].src = '<?= LINKLY_FOR_WOOCOMMERCE_PLUGIN_URL . "assets/images/password-eye-closed.svg" ?>';
+                passwordToggler.getElementsByTagName('img')[0].src = '<?= LINKLY_FOR_WOOCOMMERCE_PLUGIN_URL . "assets/images/password-eye-closed.svg" ?>';
             } else {
-                toggler.getElementsByTagName('img')[0].src = '<?= LINKLY_FOR_WOOCOMMERCE_PLUGIN_URL . "assets/images/password-eye-open.svg" ?>';
+                passwordToggler.getElementsByTagName('img')[0].src = '<?= LINKLY_FOR_WOOCOMMERCE_PLUGIN_URL . "assets/images/password-eye-open.svg" ?>';
                 clientSecret.setAttribute('type', 'password');
             }
 
-        };
+        }
 
-        setCredentialsEditable = () => {
+        function setCredentialsEditable() {
             edit = !edit;
 
             if (edit) {
                 clientId.removeAttribute('disabled');
                 clientSecret.removeAttribute('disabled');
 
-                const saveButton = document.createElement('button');
-                saveButton.setAttribute('class', 'button-primary');
-                saveButton.setAttribute('type', 'submit');
-                saveButton.innerHTML = '<?= LinklyLanguageHelper::instance()->get( "save_changes" ); ?>';
-                credentialsButton.appendChild(saveButton);
+                editButton.style.display = 'none';
+                saveButton.style.display = 'inline-block';
+                cancelButton.style.display = 'inline-block';
 
-                editButton.innerHTML = '<?= LinklyLanguageHelper::instance()->get( "cancel" ); ?>'
             } else {
                 clientId.setAttribute('disabled', 'disabled');
                 clientSecret.setAttribute('disabled', 'disabled');
 
-                if (credentialsButton.getElementsByTagName('button').length > 1) {
-                    credentialsButton.getElementsByTagName('button')[1].remove();
-                }
-
-                editButton.innerHTML = '<?= LinklyLanguageHelper::instance()->get( "edit_credentials" ); ?>';
+                editButton.style.display = 'inline-block';
+                saveButton.style.display = 'none';
+                cancelButton.style.display = 'none';
             }
         }
 
-        toggler.addEventListener('click', showHidePassword);
+        passwordToggler.addEventListener('click', showHidePassword);
         editButton.addEventListener('click', setCredentialsEditable);
+        cancelButton.addEventListener('click', setCredentialsEditable);
     </script>
 </div>
