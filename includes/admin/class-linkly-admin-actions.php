d77da8bba2e121e2aa446f62d2091d679161f0d3
msthu <m.s.thullner@gmail.com>
2023-08-05 15:59:38 +0200

    Move to admin

--- includes/admin/class-linkly-admin-actions.php
+++ includes/admin/class-linkly-admin-actions.php
@@ -9,6 +9,8 @@ class LinklyAdminActions
         add_action('admin_menu', [$this, 'register_menu'], 9999);
         add_action('admin_enqueue_scripts', [$this, 'linkly_admin_style']);
         add_action('admin_init', [$this, 'handle_save']);
+	    add_action('admin_init', [$this, 'linkly_request_token_action']);
+	    add_action('admin_init', [$this, 'linkly_request_token_callback']);
     }
 
 	public function handle_save()
@@ -42,6 +44,63 @@ class LinklyAdminActions
 	public function handle_save_button_style()
 	{
 		update_option('linkly_button_style', sanitize_text_field($_POST['linkly_button_style']));
+	}
+
+	/**
+	 * The action to redirect to the Linkly SSO server to link the webshop to Linkly
+	 *
+	 * @return void
+	 */
+	function linkly_request_token_action()
+	{
+		if (!isset($_GET['linkly_request_token'])) {
+			return;
+		}
+
+		if (!current_user_can('manage_options')) {
+			throw new Exception('User is not an admin');
+		}
+
+		$_SESSION['url_to_return_to'] = get_site_url() . urldecode($_GET['linkly_request_token']);
+		// $corsUrl is pure the domain name without the path if there is a port number it is included
+
+		$corsUrl = parse_url(get_site_url(), PHP_URL_SCHEME) . '://' . parse_url(get_site_url(), PHP_URL_HOST);
+		$port = parse_url(get_site_url(), PHP_URL_PORT);
+
+		if ($port) {
+			$corsUrl .= ':' . $port;
+		}
+
+		$params = [
+			'returnUrl' => get_site_url() . '?linkly_request_token_callback',
+			'clientName' => get_bloginfo('name'),
+			'allowedCorsOrigin' => $corsUrl,
+			'postLogoutRedirectUri' => get_site_url(),
+			'redirectUri' => get_site_url() . '?linkly-callback',
+		];
+
+		$this->ssoHelper->linkClientRedirect($params);
+		exit;
+	}
+
+	/**
+	 * The callback action after the webshop has been linked to Linkly
+	 *
+	 * @return void
+	 */
+	function linkly_request_token_callback()
+	{
+		if (!current_user_can('manage_options')) {
+			throw new Exception('User is not an admin');
+		}
+
+		$client_options = $this->ssoHelper->linkClientCallback();
+
+		update_option('linkly_settings_app_key', $client_options['client_id']);
+		update_option('linkly_settings_app_secret', $client_options['client_secret']);
+
+		wp_redirect(admin_url('admin.php?page=linkly-for-woocommerce'));
+		exit;
 	}
 
     public function register_menu()
--- includes/class-linkly-auth-actions.php
+++ includes/class-linkly-auth-actions.php
@@ -18,68 +18,10 @@ class LinklyAuthActions
         add_action('init', [$this, 'linkly_login_action']);
         add_action('init', [$this, 'linkly_link_account_action']);
         add_action('init', [$this, 'linkly_login_callback']);
-        add_action('init', [$this, 'linkly_request_token_action']);
-        add_action('init', [$this, 'linkly_request_token_callback']);
 
         add_action('wp_logout', [$this, 'linkly_logout']);
     }
 
-	/**
-	 * The action to redirect to the Linkly SSO server to link the webshop to Linkly
-	 *
-	 * @return void
-	 */
-    function linkly_request_token_action()
-    {
-        if (!isset($_GET['linkly_request_token'])) {
-            return;
-        }
-
-	    if (!current_user_can('manage_options')) {
-		    throw new Exception('User is not an admin');
-	    }
-
-        $_SESSION['url_to_return_to'] = get_site_url() . urldecode($_GET['linkly_request_token']);
-        // $corsUrl is pure the domain name without the path if there is a port number it is included
-
-        $corsUrl = parse_url(get_site_url(), PHP_URL_SCHEME) . '://' . parse_url(get_site_url(), PHP_URL_HOST);
-        $port = parse_url(get_site_url(), PHP_URL_PORT);
-
-        if ($port) {
-            $corsUrl .= ':' . $port;
-        }
-
-        $params = [
-            'returnUrl' => get_site_url() . '?linkly_request_token_callback',
-            'clientName' => get_bloginfo('name'),
-            'allowedCorsOrigin' => $corsUrl,
-            'postLogoutRedirectUri' => get_site_url(),
-            'redirectUri' => get_site_url() . '?linkly-callback',
-        ];
-
-	    $this->ssoHelper->linkClientRedirect($params);
-        exit;
-    }
-
-	/**
-	 * The callback action after the webshop has been linked to Linkly
-	 *
-	 * @return void
-	 */
-    function linkly_request_token_callback()
-    {
-	    if (!current_user_can('manage_options')) {
-		    throw new Exception('User is not an admin');
-	    }
-
-		$client_options = $this->ssoHelper->linkClientCallback();
-
-        update_option('linkly_settings_app_key', $client_options['client_id']);
-        update_option('linkly_settings_app_secret', $client_options['client_secret']);
-
-        wp_redirect(admin_url('admin.php?page=linkly-for-woocommerce'));
-        exit;
-    }
 
 	/**
 	 * The action to redirect to the Linkly SSO server to link the WordPress user to Linkly
