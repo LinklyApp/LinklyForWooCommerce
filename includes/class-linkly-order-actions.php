1a8452ab8765f5fd509359e5fbb5f8e257550835
msthu <m.s.thullner@gmail.com>
2023-08-11 14:48:00 +0200

    Add sync

--- includes/class-linkly-order-actions.php
+++ includes/class-linkly-order-actions.php
@@ -5,105 +5,141 @@ use Linkly\OAuth2\Client\Helpers\LinklyOrderHelper;
 use Linkly\OAuth2\Client\Helpers\LinklySsoHelper;
 use Linkly\OAuth2\Client\Provider\Exception\LinklyProviderException;
 
-if ( ! defined( 'ABSPATH' ) ) exit; // Exit if accessed directly
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+} // Exit if accessed directly
 
-class LinklyOrderActions
-{
-    /**
-     * @var LinklyOrderHelper
-     */
-    private $linklyOrderHelper;
+class LinklyOrderActions {
+	/**
+	 * @var LinklyOrderHelper
+	 */
+	private LinklyOrderHelper $linklyOrderHelper;
 
-    /**
-     * @var LinklySsoHelper
-     */
-    private $linklySsoHelper;
+	/**
+	 * @var string The status name for when the order is processing
+	 */
+	private string $processing_status_name = "Processing";
 
-    /**
-     * @var string The status name for when the order is processing
-     */
-    private string $processing_status_name = "Processing";
+	/**
+	 * @var string The status name for when the order is completed
+	 */
+	private string $completed_status_name = "Completed";
 
-    /**
-     * @var string The status name for when the order is completed
-     */
-    private string $completed_status_name = "Completed";
+	public function __construct( LinklyOrderHelper $linklyOrderHelper ) {
+		$this->linklyOrderHelper = $linklyOrderHelper;
 
-    public function __construct(LinklyOrderHelper $linklyOrderHelper,
-                                LinklySsoHelper   $linklySsoHelper)
-    {
-        $this->linklyOrderHelper = $linklyOrderHelper;
-        $this->linklySsoHelper = $linklySsoHelper;
+		add_action( 'woocommerce_account_dashboard', [ $this, 'sync_current_wc_customer_invoices_to_linkly' ], 999 );
+		add_action( 'woocommerce_after_account_orders', [ $this, 'sync_current_wc_customer_invoices_to_linkly' ], 999 );
+		add_action( 'linkly_after_link_wc_account', [ $this, 'sync_current_wc_customer_invoices_to_linkly' ] );
+		add_action( 'woocommerce_order_status_processing', [ $this, 'linkly_order_to_processing' ] );
+		add_action( 'woocommerce_order_status_completed', [ $this, 'linkly_order_to_completed' ] );
+	}
 
-        add_action('woocommerce_account_dashboard', [$this, 'sync_current_wc_customer_invoices_to_linkly'], 999);
-        add_action('woocommerce_after_account_orders', [$this, 'sync_current_wc_customer_invoices_to_linkly'], 999);
-        // TODO - change to status_completed
-        add_action('woocommerce_order_status_processing', [$this, 'linkly_order_to_processing']);
-        add_action('woocommerce_order_status_completed', [$this, 'linkly_order_to_completed']);
-    }
+	function linkly_order_to_processing( $order_id ) {
+		try {
+			$order = wc_get_order( $order_id );
 
-    function linkly_order_to_processing($order_id)
-    {
-        try {
-            $order = wc_get_order($order_id);
+			$customer = new WC_Customer( $order->get_user_id() );
 
-	        $customer = new WC_Customer($order->get_user_id());
+			if ( ! $customer->get_meta( 'linkly_user' ) ) {
+				return;
+			}
 
-	        if (!$customer->get_meta('linkly_user')) {
-		        return;
-	        }
+			$orderData = LinklyWCOrderToLinklyOrderMapper::mapOrder( $order, $this->processing_status_name );
+			$this->linklyOrderHelper->sendOrder( $orderData );
+			$order->add_meta_data( 'linkly_exported', gmdate( "Y-m-d H:i:s" ) . ' +00:00' );
+			$order->save();
+		} catch ( LinklyProviderException $e ) {
+			linkly_dd( $e->getResponseBody() );
+		} catch ( IdentityProviderException $e ) {
+			linkly_dd( [ $e->getResponseBody() ] );
+		}
+	}
 
-            $orderData = LinklyWCOrderToLinklyOrderMapper::mapOrder($order, $this->processing_status_name);
-            $this->linklyOrderHelper->sendOrder($orderData);
-            $order->add_meta_data('linkly_exported', gmdate("Y-m-d H:i:s") . ' +00:00');
-            $order->save();
-        } catch (LinklyProviderException $e) {
-            linkly_dd($e->getResponseBody());
-        } catch (IdentityProviderException $e) {
-            linkly_dd([$e->getResponseBody()]);
-        }
-    }
+	function linkly_order_to_completed( $order_id ) {
+		try {
+			$order    = wc_get_order( $order_id );
+			$customer = new WC_Customer( $order->get_user_id() );
 
-    function linkly_order_to_completed($order_id)
-    {
-        try {
-            $order = wc_get_order($order_id);
-            $customer = new WC_Customer($order->get_user_id());
+			if ( ! $customer->get_meta( 'linkly_user' ) ) {
+				return;
+			}
 
-            if (!$customer->get_meta('linkly_user')) {
-                return;
-            }
+			$orderData = LinklyWCOrderToLinklyOrderMapper::mapOrder( $order, $this->completed_status_name );
+			$this->linklyOrderHelper->sendOrder( $orderData );
 
-            $orderData = LinklyWCOrderToLinklyOrderMapper::mapOrder($order, $this->completed_status_name);
-            $this->linklyOrderHelper->sendOrder($orderData);
-
-			if (linkly_is_pdf_invoices_plugin_active()) {
+			if ( linkly_is_pdf_invoices_plugin_active() ) {
 				$orderDocument = wcpdf_get_document( 'invoice', $order, true );
 				$invoiceData   = LinklyWCInvoiceToLinklyInvoiceMapper::mapInvoice( $order, $orderDocument );
 				$this->linklyOrderHelper->sendInvoice( $invoiceData );
 			}
 
-            $order->add_meta_data('linkly_exported', gmdate("Y-m-d H:i:s") . ' +00:00');
-            $order->save();
-        } catch (LinklyProviderException $e) {
-            error_log($e->getMessage());
-        } catch (Exception $e) {
-            error_log($order, $e->getMessage());
-        }
-    }
+			$order->add_meta_data( 'linkly_exported', gmdate( "Y-m-d H:i:s" ) . ' +00:00' );
+			$order->save();
+		} catch ( LinklyProviderException $e ) {
+			error_log( $e->getMessage() );
+		} catch ( Exception $e ) {
+			error_log( $order, $e->getMessage() );
+		}
+	}
 
-    public function sync_current_wc_customer_invoices_to_linkly(): void
-    {
-		// TODO - Vervang ssohelper isauthenticated
-        if (is_user_logged_in()) {
-            $customer = new WC_Customer(get_current_user_id());
+	public function sync_current_wc_customer_invoices_to_linkly(): void {
+		if ( ! is_user_logged_in() ) {
+			return;
+		}
 
-	        if (!$customer->get_meta('linkly_user')) {
-		        return;
-	        }
-            linkly_sync_customer_invoices($customer);
-        }
-    }
+		$customer = new WC_Customer( get_current_user_id() );
+
+		if ( ! $customer->get_meta( 'linkly_user' ) ) {
+			return;
+		}
+
+		$this->sync_customer_orders_and_invoices( $customer );
+	}
+
+
+	/**
+	 * @param WC_Customer $customer
+	 *
+	 * @return void
+	 */
+	private function sync_customer_orders_and_invoices( WC_Customer $customer ): void {
+		$args = array(
+			'limit'        => - 1, // Limit of orders to retrieve
+			'meta_key'     => 'linkly_order_exported', // Postmeta key field
+			'meta_compare' => 'NOT EXISTS',
+			'customer_id'  => $customer->get_id(), // User ID
+			'return'       => 'objects', // Possible values are ‘ids’ and ‘objects’.
+		);
+
+		$orders = wc_get_orders( $args );
+
+		foreach ( $orders as $order ) {
+			try {
+				$orderData = LinklyWCOrderToLinklyOrderMapper::mapOrder( $order, $order->get_status() );
+				$this->linklyOrderHelper->sendOrder( $orderData );
+				$order->add_meta_data( 'linkly_order_exported', gmdate( "Y-m-d H:i:s" ) . ' +00:00' );
+				$order->save();
+
+				if ( $order->get_status() !== 'completed' || ! linkly_is_pdf_invoices_plugin_active() ) {
+					continue;
+				}
+
+				$invoice     = wcpdf_get_document( 'invoice', $order, true );
+				$invoiceData = LinklyWCInvoiceToLinklyInvoiceMapper::mapInvoice( $order, $invoice );
+				$this->linklyOrderHelper->sendInvoice( $invoiceData );
+
+				$order->add_meta_data( 'linkly_invoice_exported', gmdate( "Y-m-d H:i:s" ) . ' +00:00' );
+				$order->save();
+			} catch ( LinklyProviderException $e ) {
+				dd( $e->getResponseBody() );
+				error_log( json_encode( $e->getResponseBody() ) );
+			} catch ( Exception $e ) {
+				error_log( json_encode( $e->getMessage() ) );
+			}
+		}
+	}
+
 }
 
-new LinklyOrderActions(LinklyHelpers::instance()->getInvoiceHelper(), LinklyHelpers::instance()->getSsoHelper());
+new LinklyOrderActions( LinklyHelpers::instance()->getOrderHelper() );
--- includes/linkly-functions.php
+++ includes/linkly-functions.php
@@ -10,15 +10,12 @@ if ( ! defined( 'ABSPATH' ) ) {
 
 /**
  * @param LinklyUser $linklyUser
- * @param WP_User|null $currentUser
+ * @param WC_Customer $customer
  *
  * @return void
- * @throws Exception
  */
-function linkly_createOrUpdateCustomer( LinklyUser $linklyUser, WP_User $currentUser = null ) {
+function linkly_update_wc_customer( LinklyUser $linklyUser, WC_Customer $customer ) {
 	$mappedCustomer = LinklyCustomerToWCCustomerMapper::map( $linklyUser );
-	$customer       = new WC_Customer( $currentUser->ID );
-
 	$customer->set_props( $mappedCustomer );
 
 	$customer->add_meta_data( 'linkly_user', true, true );
@@ -28,79 +25,6 @@ function linkly_createOrUpdateCustomer( LinklyUser $linklyUser, WP_User $current
 	$customer->update_meta_data( 'linkly_shipping_version', $linklyUser->getShippingAddress()->getVersion() );
 
 	$customer->save();
-
-	linkly_login_user( $customer );
-}
-
-/**
- * @param LinklyUser $linklyUser
- * @param WP_User $currentUser
- *
- * @return void
- * @throws Exception
- */
-function linkly_attachWCCustomer( LinklyUser $linklyUser, WP_User $currentUser ) {
-	$mappedCustomer = LinklyCustomerToWCCustomerMapper::map( $linklyUser );
-
-	if ( $currentUser->ID !== 0 && $linklyUser->getEmail() !== $currentUser->user_email && ! user_can( $currentUser, 'manage_options' ) ) {
-		$currentUser->user_email = $linklyUser->getEmail();
-		$response                = wp_update_user( $currentUser );
-		if ( is_wp_error( $response ) ) {
-			throw new Exception( $response->get_error_message() );
-		}
-	}
-
-	$customer = new WC_Customer( $currentUser->ID );
-
-	$customer->set_props( $mappedCustomer );
-	$customer->add_meta_data( 'linkly_user', true, true );
-	$customer->save();
-
-	linkly_sync_customer_invoices( $customer );
-}
-
-/**
- * @param WC_Customer $customer
- *
- * @return void
- */
-function linkly_sync_customer_invoices( WC_Customer $customer ): void {
-	$args = array(
-		'limit'        => - 1, // Limit of orders to retrieve
-		'meta_key'     => 'linkly_order_exported', // Postmeta key field
-		'meta_compare' => 'NOT EXISTS',
-		'customer_id'  => $customer->get_id(), // User ID
-		'return'       => 'objects', // Possible values are ‘ids’ and ‘objects’.
-	);
-
-	$orders = wc_get_orders( $args );
-
-	$linklyOrderHelper = LinklyHelpers::instance()->getInvoiceHelper();
-
-	foreach ( $orders as $order ) {
-		try {
-			$orderData = LinklyWCOrderToLinklyOrderMapper::mapOrder( $order, $order->get_status());
-			$linklyOrderHelper->sendOrder( $orderData );
-			$order->add_meta_data( 'linkly_order_exported', gmdate( "Y-m-d H:i:s" ) . ' +00:00' );
-			$order->save();
-
-			if ( $order->get_status() !== 'completed' || ! linkly_is_pdf_invoices_plugin_active() ) {
-				continue;
-			}
-
-			$invoice     = wcpdf_get_document( 'invoice', $order, true );
-			$invoiceData = LinklyWCInvoiceToLinklyInvoiceMapper::mapInvoice( $order, $invoice );
-			$linklyOrderHelper->sendInvoice( $invoiceData );
-
-			$order->add_meta_data( 'linkly_invoice_exported', gmdate( "Y-m-d H:i:s" ) . ' +00:00' );
-			$order->save();
-		} catch ( LinklyProviderException $e ) {
-			dd( $e->getResponseBody());
-			error_log( json_encode($e->getResponseBody()) );
-		} catch ( Exception $e ) {
-			error_log( json_encode($e->getMessage()) );
-		}
-	}
 }
 
 function linkly_is_pdf_invoices_plugin_active() {
@@ -141,21 +65,6 @@ function linkly_is_billing_address_equal_to_shipping_address( WC_Customer $custo
 	return $customer->get_billing_address_1() === $customer->get_shipping_address_1()
 	       && $customer->get_billing_first_name() === $customer->get_shipping_first_name()
 	       && $customer->get_billing_last_name() === $customer->get_shipping_last_name();
-}
-
-/**
- * @return string
- */
-function linkly_getBaseUrl() {
-	$env = get_option( 'linkly_settings_environment' );
-	if ( $env === 'local' ) {
-		return LinklyHelpers::instance()->getLinklyProvider()->localDomain;
-	}
-	if ( $env === 'beta' ) {
-		return LinklyHelpers::instance()->getLinklyProvider()->betaDomain;
-	}
-
-	return LinklyHelpers::instance()->getLinklyProvider()->domain;
 }
 
 function linkly_dd( $variable ) {
