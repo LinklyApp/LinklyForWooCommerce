356cbc216524be86e4e04e394b2994b6b0b7cbb6
msthu <m.s.thullner@gmail.com>
2023-08-11 15:25:55 +0200

    refactor order and invoice sending

--- includes/class-linkly-address-actions.php
+++ includes/class-linkly-address-actions.php
@@ -80,7 +80,7 @@ class LinklyAddressActions
 		}
 
 		$customer = new WC_Customer(get_current_user_id());
-		if (!$customer->get_meta('linkly_user')) {
+		if (!linkly_is_wp_user_linkly_user($customer->get_id())) {
 			return;
 		}
 
--- includes/class-linkly-order-actions.php
+++ includes/class-linkly-order-actions.php
@@ -35,52 +35,12 @@ class LinklyOrderActions {
 		add_action( 'woocommerce_order_status_completed', [ $this, 'linkly_order_to_completed' ] );
 	}
 
-	function linkly_order_to_processing( $order_id ) {
-		try {
-			$order = wc_get_order( $order_id );
-
-			$customer = new WC_Customer( $order->get_user_id() );
-
-			if ( ! $customer->get_meta( 'linkly_user' ) ) {
-				return;
-			}
-
-			$orderData = LinklyWCOrderToLinklyOrderMapper::mapOrder( $order, $this->processing_status_name );
-			$this->linklyOrderHelper->sendOrder( $orderData );
-			$order->add_meta_data( 'linkly_exported', gmdate( "Y-m-d H:i:s" ) . ' +00:00' );
-			$order->save();
-		} catch ( LinklyProviderException $e ) {
-			linkly_dd( $e->getResponseBody() );
-		} catch ( IdentityProviderException $e ) {
-			linkly_dd( [ $e->getResponseBody() ] );
-		}
+	public function linkly_order_to_processing( $order_id ) {
+		$this->send_linkly_order_and_invoice_to_linkly( $order_id, $this->processing_status_name );
 	}
 
-	function linkly_order_to_completed( $order_id ) {
-		try {
-			$order    = wc_get_order( $order_id );
-			$customer = new WC_Customer( $order->get_user_id() );
-
-			if ( ! $customer->get_meta( 'linkly_user' ) ) {
-				return;
-			}
-
-			$orderData = LinklyWCOrderToLinklyOrderMapper::mapOrder( $order, $this->completed_status_name );
-			$this->linklyOrderHelper->sendOrder( $orderData );
-
-			if ( linkly_is_pdf_invoices_plugin_active() ) {
-				$orderDocument = wcpdf_get_document( 'invoice', $order, true );
-				$invoiceData   = LinklyWCInvoiceToLinklyInvoiceMapper::mapInvoice( $order, $orderDocument );
-				$this->linklyOrderHelper->sendInvoice( $invoiceData );
-			}
-
-			$order->add_meta_data( 'linkly_exported', gmdate( "Y-m-d H:i:s" ) . ' +00:00' );
-			$order->save();
-		} catch ( LinklyProviderException $e ) {
-			error_log( $e->getMessage() );
-		} catch ( Exception $e ) {
-			error_log( $order, $e->getMessage() );
-		}
+	public function linkly_order_to_completed( $order_id ) {
+		$this->send_linkly_order_and_invoice_to_linkly( $order_id, $this->completed_status_name, true );
 	}
 
 	public function sync_current_wc_customer_invoices_to_linkly(): void {
@@ -90,14 +50,18 @@ class LinklyOrderActions {
 
 		$customer = new WC_Customer( get_current_user_id() );
 
-		if ( ! $customer->get_meta( 'linkly_user' ) ) {
+		if ( ! linkly_is_wp_user_linkly_user($customer->get_id()) ) {
 			return;
 		}
 
 		$this->sync_customer_orders_and_invoices( $customer );
 	}
 
-
+	/**
+	 * @param WC_Customer $customer
+	 *
+	 * @return void
+	 */
 	/**
 	 * @param WC_Customer $customer
 	 *
@@ -105,7 +69,7 @@ class LinklyOrderActions {
 	 */
 	private function sync_customer_orders_and_invoices( WC_Customer $customer ): void {
 		$args = array(
-			'limit'        => - 1, // Limit of orders to retrieve
+			'limit'        => -1, // Limit of orders to retrieve
 			'meta_key'     => 'linkly_order_exported', // Postmeta key field
 			'meta_compare' => 'NOT EXISTS',
 			'customer_id'  => $customer->get_id(), // User ID
@@ -115,28 +79,51 @@ class LinklyOrderActions {
 		$orders = wc_get_orders( $args );
 
 		foreach ( $orders as $order ) {
-			try {
-				$orderData = LinklyWCOrderToLinklyOrderMapper::mapOrder( $order, $order->get_status() );
-				$this->linklyOrderHelper->sendOrder( $orderData );
-				$order->add_meta_data( 'linkly_order_exported', gmdate( "Y-m-d H:i:s" ) . ' +00:00' );
-				$order->save();
+			$status_name = $order->get_status();
+			$handle_invoice = ($status_name === 'completed');
 
-				if ( $order->get_status() !== 'completed' || ! linkly_is_pdf_invoices_plugin_active() ) {
-					continue;
-				}
+			$this->send_linkly_order_and_invoice_to_linkly( $order->get_id(), $status_name, $handle_invoice);
 
-				$invoice     = wcpdf_get_document( 'invoice', $order, true );
-				$invoiceData = LinklyWCInvoiceToLinklyInvoiceMapper::mapInvoice( $order, $invoice );
-				$this->linklyOrderHelper->sendInvoice( $invoiceData );
-
+			if ($handle_invoice && ! $order->get_meta('linkly_invoice_exported')) {
 				$order->add_meta_data( 'linkly_invoice_exported', gmdate( "Y-m-d H:i:s" ) . ' +00:00' );
 				$order->save();
-			} catch ( LinklyProviderException $e ) {
-				dd( $e->getResponseBody() );
-				error_log( json_encode( $e->getResponseBody() ) );
-			} catch ( Exception $e ) {
-				error_log( json_encode( $e->getMessage() ) );
 			}
+		}
+	}
+
+	/**
+	 * @param int $order_id
+	 * @param string $status_name
+	 * @param bool $handle_invoice
+	 *
+	 * @return void
+	 */
+	private function send_linkly_order_and_invoice_to_linkly( $order_id, $status_name, $handle_invoice = false ) {
+		try {
+			$order    = wc_get_order( $order_id );
+			$customer = new WC_Customer( $order->get_user_id() );
+
+			if ( ! linkly_is_wp_user_linkly_user($customer->get_id()) ) {
+				return;
+			}
+
+			$orderData = LinklyWCOrderToLinklyOrderMapper::mapOrder( $order, $status_name );
+			$this->linklyOrderHelper->sendOrder( $orderData );
+
+			if ( $handle_invoice && linkly_is_pdf_invoices_plugin_active() ) {
+				$orderDocument = wcpdf_get_document( 'invoice', $order, true );
+				$invoiceData   = LinklyWCInvoiceToLinklyInvoiceMapper::mapInvoice( $order, $orderDocument );
+				$this->linklyOrderHelper->sendInvoice( $invoiceData );
+			}
+
+			$order->add_meta_data( 'linkly_exported', gmdate( "Y-m-d H:i:s" ) . ' +00:00' );
+			$order->save();
+		} catch ( LinklyProviderException $e ) {
+			error_log( json_encode($e->getResponseBody()) );
+		} catch ( IdentityProviderException $e ) {
+			error_log( json_encode($e->getResponseBody()) );
+		} catch ( Exception $e ) {
+			error_log( json_encode($e->getMessage()) );
 		}
 	}
 
