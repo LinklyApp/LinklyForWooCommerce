5377134dd019273b92c3853456888b91bfa209df
msthu <m.s.thullner@gmail.com>
2023-09-27 14:40:23 +0200

    Remove variables from translation echo

--- includes/admin/views/html-admin-page.php
+++ includes/admin/views/html-admin-page.php
@@ -7,18 +7,16 @@ $buttonStyle   = get_option( 'linkly_button_style' );
 $logoStyle     = $buttonStyle === 'primary' ? 'light' : 'dark';
 $linklyHelpers = LinklyHelpers::instance();
 
-$textDomain = 'linkly-for-woocommerce';
-
 ?>
 
 <div id="clientSecretModal" class="modal">
     <div class="modal-content">
         <form action="<?php echo esc_url( remove_query_arg( 'client_id' ) ); ?>" method="post">
-            <h2><?php esc_html_e( "enter-client-secret", $textDomain ); ?></h2>
+            <h2><?php esc_html_e( "enter-client-secret", 'linkly-for-woocommerce' ); ?></h2>
 
             <div class="linkly-form-group">
                 <label class="linkly-form-label" for="linkly_modal_client_id">
-					<?php esc_html_e( "client.id", $textDomain ); ?>
+					<?php esc_html_e( "client.id", 'linkly-for-woocommerce' ); ?>
                 </label>
                 <input name="linkly_client_id" id="linkly_modal_client_id" class="linkly-form-input" type="text"
                        value="<?php echo isset( $_GET['client_id'] ) ? esc_html( $_GET['client_id'] ) : ''; ?>"
@@ -27,7 +25,7 @@ $textDomain = 'linkly-for-woocommerce';
 
             <div class="linkly-form-group">
                 <label class="linkly-form-label" for="linkly_modal_client_secret">
-					<?php esc_html_e( "client.secret", $textDomain ); ?>
+					<?php esc_html_e( "client.secret", 'linkly-for-woocommerce' ); ?>
                 </label>
                 <input name="linkly_client_secret" id="linkly_modal_client_secret" class="linkly-form-input"
                        type="text"/>
@@ -36,10 +34,10 @@ $textDomain = 'linkly-for-woocommerce';
 			<?php wp_nonce_field( 'linkly_credentials' ); ?>
 
             <input type="submit" name="submit_client_secret" class="button-primary"
-                   value="<?php esc_html_e( "save_changes", $textDomain ); ?>">
+                   value="<?php esc_html_e( "save_changes", 'linkly-for-woocommerce' ); ?>">
             <button onclick="window.location.href = '<?php echo esc_js( esc_url( remove_query_arg( 'client_id' ) ) ); ?>'; return false;"
                     class="button-secondary" type="button">
-				<?php esc_html_e( "cancel", $textDomain ); ?>
+				<?php esc_html_e( "cancel", 'linkly-for-woocommerce' ); ?>
             </button>
         </form>
     </div>
@@ -53,32 +51,32 @@ $textDomain = 'linkly-for-woocommerce';
             <div class="linkly-warning">
                 <img src="<?php echo esc_url( LINKLY_FOR_WOOCOMMERCE_PLUGIN_URL . 'assets/images/package_warning.svg' ) ?>">
                 <div class="linkly-warning-description">
-					<?php esc_html_e( "warning.pdf-invoice-plugin-not-activated", $textDomain ) ?>
+					<?php esc_html_e( "warning.pdf-invoice-plugin-not-activated", 'linkly-for-woocommerce' ) ?>
                 </div>
             </div>
 		<?php endif; ?>
 		<?php do_action( 'linkly_notice_hook' ); ?>
         <div class="linkly-admin-page__row">
 			<?php if ( $linklyHelpers->isConnectedWithVerification() ) : ?>
-                <h3><?php esc_html_e( "client.status", $textDomain ) ?>:
+                <h3><?php esc_html_e( "client.status", 'linkly-for-woocommerce' ) ?>:
                     <span class="client-status client-status__connected">
-                        <?php esc_html_e( "client.connected", $textDomain ) ?>
+                        <?php esc_html_e( "client.connected", 'linkly-for-woocommerce' ) ?>
                     </span></h3>
                 <a class="button"
                    href="<?php echo esc_url( $linklyHelpers->getLinklyProvider()->getWebDomainUrl() ) ?>"
-                   target="_blank"><?php esc_html_e( "go-to-linkly-button", $textDomain ) ?>
+                   target="_blank"><?php esc_html_e( "go-to-linkly-button", 'linkly-for-woocommerce' ) ?>
 
                 </a>
 			<?php endif; ?>
 			<?php if ( ! $linklyHelpers->isConnectedWithVerification() ) : ?>
-                <h3><?php esc_html_e( "client.status", $textDomain ) ?>:
+                <h3><?php esc_html_e( "client.status", 'linkly-for-woocommerce' ) ?>:
                     <span class="client-status client-status__disconnected">
-                        <?php esc_html_e( "client.disconnected", $textDomain ) ?>
+                        <?php esc_html_e( "client.disconnected", 'linkly-for-woocommerce' ) ?>
                     </span></h3>
                 <form method="post" style="margin: 0">
 					<?php wp_nonce_field( 'linkly_admin_connect' ); ?>
                     <button class="button-primary" type="submit">
-						<?php esc_html_e( "admin-connect-button", $textDomain ) ?>
+						<?php esc_html_e( "admin-connect-button", 'linkly-for-woocommerce' ) ?>
                     </button>
                 </form>
 			<?php endif; ?>
@@ -87,19 +85,19 @@ $textDomain = 'linkly-for-woocommerce';
 
 			<?php wp_nonce_field( 'linkly_credentials' ); ?>
             <div style="flex-basis: 100%">
-                <h3><?php esc_html_e( "client.connection-settings", $textDomain ); ?>
+                <h3><?php esc_html_e( "client.connection-settings", 'linkly-for-woocommerce' ); ?>
                 </h3>
 
                 <div class="linkly-form-group">
                     <label class="linkly-form-label" for="linkly_client_id">
-						<?php esc_html_e( "client.id", $textDomain ); ?>
+						<?php esc_html_e( "client.id", 'linkly-for-woocommerce' ); ?>
                     </label>
                     <input name="linkly_client_id" id="linkly_client_id" class="linkly-form-input" type="text"
                            value="<?php echo esc_html( get_option( 'linkly_settings_app_key' ) ) ?>" disabled/>
                 </div>
                 <div class="linkly-form-group">
                     <label class="linkly-form-label" for="linkly_client_secret">
-						<?php esc_html_e( "client.secret", $textDomain ); ?>
+						<?php esc_html_e( "client.secret", 'linkly-for-woocommerce' ); ?>
                     </label>
                     <input name="linkly_client_secret" id="linkly_client_secret" class="linkly-form-input"
                            type="password"
@@ -110,13 +108,13 @@ $textDomain = 'linkly-for-woocommerce';
 
                 <div id="linkly_credentials_button" class="linkly-credentials-button">
                     <button id="linkly_edit_credential_button" class="button-primary" type="button">
-			            <?php esc_html_e( "edit_credentials", $textDomain ); ?>
+			            <?php esc_html_e( "edit_credentials", 'linkly-for-woocommerce' ); ?>
                     </button>
                     <button id="linkly_save_credentials_button" class="button-primary" type="submit">
-			            <?php esc_html_e( "save_changes", $textDomain ); ?>
+			            <?php esc_html_e( "save_changes", 'linkly-for-woocommerce' ); ?>
                     </button>
                     <button id="linkly_cancel_edit_credential_button" class="button-secondary" type="button">
-			            <?php esc_html_e( "cancel", $textDomain ); ?>
+			            <?php esc_html_e( "cancel", 'linkly-for-woocommerce' ); ?>
                     </button>
                 </div>
             </div>
@@ -125,12 +123,12 @@ $textDomain = 'linkly-for-woocommerce';
         <form method="post" id="linklyButtonForm" class="linkly-admin-page__row">
 			<?php wp_nonce_field( 'linkly_button_style' ); ?>
             <div style="display: flex; justify-content: start; align-items: start">
-                <h3><?php esc_html_e( 'button_style.title', $textDomain ) ?></h3></div>
+                <h3><?php esc_html_e( 'button_style.title', 'linkly-for-woocommerce' ) ?></h3></div>
             <div class="linkly-button__wrapper <?php get_option( 'linkly_button_style' ) === 'primary' ? esc_attr_e( 'selected' ) : esc_attr_e( '' ) ?>">
                 <div class="linkly-button  primary">
 
                     <a href="javascript:void(0);">
-                        <span><?php esc_html_e( "button_style.primary", $textDomain ) ?></span>
+                        <span><?php esc_html_e( "button_style.primary", 'linkly-for-woocommerce' ) ?></span>
                         <img src="<?php echo esc_url( LINKLY_FOR_WOOCOMMERCE_PLUGIN_URL . "assets/images/logo-horizontal-light.svg" ) ?>"
                              alt="Linkly">
                     </a>
@@ -139,7 +137,7 @@ $textDomain = 'linkly-for-woocommerce';
             <div class="linkly-button__wrapper <?php get_option( 'linkly_button_style' ) === 'secondary' ? esc_attr_e( 'selected' ) : esc_attr_e( '' ) ?>">
                 <div class="linkly-button secondary">
                     <a href="javascript:void(0);">
-                        <span><?php esc_html_e( "button_style.secondary", $textDomain ) ?></span>
+                        <span><?php esc_html_e( "button_style.secondary", 'linkly-for-woocommerce' ) ?></span>
                         <img src="<?php echo esc_url( LINKLY_FOR_WOOCOMMERCE_PLUGIN_URL . "assets/images/logo-horizontal-dark.svg" ) ?>"
                              alt="Linkly">
                     </a>
--- includes/parts/views/html-buttons.php
+++ includes/parts/views/html-buttons.php
@@ -9,23 +9,22 @@ $buttonVariant = "";
 $showButton    = true;
 $buttonStyle   = get_option( 'linkly_button_style' );
 $logoStyle     = $buttonStyle === 'primary' ? 'light' : 'dark';
-$textDomain    = 'linkly-for-woocommerce';
 
 if ( ! isset( $onlyLinkButton ) ) {
 	$onlyLinkButton = false;
 }
 
-$rawCurrentUri = $_SERVER['REQUEST_URI'];
-$sanitizedCurrentUri  = filter_var($rawCurrentUri, FILTER_SANITIZE_URL);
+$rawCurrentUri       = $_SERVER['REQUEST_URI'];
+$sanitizedCurrentUri = filter_var( $rawCurrentUri, FILTER_SANITIZE_URL );
 
 if ( is_user_logged_in() && linkly_is_wp_user_linkly_user( get_current_user_id() ) && ! $onlyLinkButton ) {
-	$buttonUrl  = '?linkly_change_address_action=' . urlencode( $sanitizedCurrentUri );
+	$buttonUrl     = '?linkly_change_address_action=' . urlencode( $sanitizedCurrentUri );
 	$buttonVariant = 'change-address-button';
 } else if ( is_user_logged_in() && ! linkly_is_wp_user_linkly_user( get_current_user_id() ) ) {
-	$buttonUrl  = '?linkly_link_account_action=' . urlencode( $sanitizedCurrentUri );
+	$buttonUrl     = '?linkly_link_account_action=' . urlencode( $sanitizedCurrentUri );
 	$buttonVariant = 'link-account-button';
 } else if ( ! $onlyLinkButton ) {
-	$buttonUrl  = '?linkly_login_action=' . urlencode( $sanitizedCurrentUri );
+	$buttonUrl     = '?linkly_login_action=' . urlencode( $sanitizedCurrentUri );
 	$buttonVariant = 'login-button';
 } else {
 	$showButton = false;
@@ -36,7 +35,21 @@ if ( is_user_logged_in() && linkly_is_wp_user_linkly_user( get_current_user_id()
     <div id="linkly-sso-button" class="linkly-sso-button">
         <div class="linkly-button <?php echo esc_attr( $buttonStyle ) ?>">
             <a href="<?php echo esc_url( $buttonUrl ) ?>">
-                <span><?php esc_html_e( $buttonVariant, $textDomain ) ?></span>
+                <span>
+                    <?php
+                    switch ( $buttonVariant ) {
+                        case 'change-address-button':
+                            esc_html_e( 'Change Address Button', 'linkly-for-woocommerce' );
+                            break;
+                        case 'link-account-button':
+                            esc_html_e( 'Link Account Button', 'linkly-for-woocommerce' );
+                            break;
+                        case 'login-button':
+                            esc_html_e( 'Login Button', 'linkly-for-woocommerce' );
+                            break;
+                    }
+                    ?>
+                </span>
                 <img src="<?php echo esc_url( LINKLY_FOR_WOOCOMMERCE_PLUGIN_URL .
 				                              'assets/images/logo-horizontal-' . $logoStyle . '.svg' ); ?>"
                      alt="Linkly">
